3e190a3532f93317ec42a5fc3fdc150823b8e908
Nifemi Bamgbose <nifemibams@gmail.com>
2022-07-01 21:48:50 +0100

    new

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +123,7 @@ STATICFILES_DIRS=[BASE_DIR/"static"]
 STATIC_ROOT= BASE_DIR/'static_cdn'
 MEDIA_URL = "/media/"
 MEDIA_ROOT= BASE_DIR/"media"
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
